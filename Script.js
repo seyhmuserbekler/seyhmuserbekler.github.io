43bfeea1e9f00b2df05bbc074ba7df9441217115
ŞEYHMUS ERBEKLER <seyhmuserbekler@gmail.com>
2025-03-03 14:24:12 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,5 @@
-
+   
+    <script>
+        gsap.from(".anim-title", { duration: 1, y: -50, opacity: 0, ease: "power2.out" });
+        gsap.from(".anim-subtitle", { duration: 1, delay: 0.5, y: 50, opacity: 0, ease: "power2.out" });
+    </script>
