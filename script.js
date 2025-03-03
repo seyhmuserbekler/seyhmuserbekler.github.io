b9bff537316ae7c3adb7b24b1500ab8ab09ad8e5
ŞEYHMUS ERBEKLER <seyhmuserbekler@gmail.com>
2025-03-03 14:56:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.from(".anim-title", { duration: 1, y: -50, opacity: 0, ease: "power2.out" });
+    gsap.from(".anim-subtitle", { duration: 1, delay: 0.5, y: 50, opacity: 0, ease: "power2.out" });
+});
